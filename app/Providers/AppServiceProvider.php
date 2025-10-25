371fe39c9ccbc6d1857cc2688e7e4595894b2e4d
Charl <charldominicursabia@gmail.com>
2025-10-25 13:37:22 +0800

    third commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Only register Debugbar in development environment
+        if ($this->app->environment('local') && config('app.debug')) {
+            if (class_exists(\Barryvdh\Debugbar\ServiceProvider::class)) {
+                $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            }
+        }
     }
 
     /**
